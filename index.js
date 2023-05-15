a6c05b1e24b7c3796b114f087fe3352b297292bd
nurullah91 <mdnurullah.ptk@gmail.com>
2023-05-15 17:32:32 +0600

    cart data loaded from server

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const port = process.env.PORT || 5000;
 
 // middleWare
 app.use(cors())
-app.use(express())
+app.use(express.json())
 
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gspcn8d.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,14 +31,31 @@ async function run() {
 
 
         app.get('/products', async(req, res) =>{
-            const result = await productCollection.find().toArray();
+            console.log(req.query);
+            const page = parseInt(req.query.page) || 0;
+            const limit = parseInt(req.query.limit) || 10;
+            const skip = page * limit;
 
+
+
+            const result = await productCollection.find().skip(skip).limit(limit).toArray();
             res.send(result);
         })
 
         app.get('/totalProducts', async(req, res)=>{
             const result = await productCollection.estimatedDocumentCount();
             res.send({totalProducts: result});
+        })
+
+        app.post('/productsById', async(req, res) =>{
+            const ids = req.body;
+            console.log(ids);
+
+            const objectIds = ids.map(id => new ObjectId(id));
+            const query = {_id: {$in: objectIds}}
+            
+            const result = await productCollection.find(query).toArray();
+            res.send(result);
         })
 
 
