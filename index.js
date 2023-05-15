4d159d584a8b52c7f062717dfbcbde7dcf2167db
nurullah91 <mdnurullah.ptk@gmail.com>
2023-05-15 10:17:24 +0600

    total products ruts added

--- index.js
+++ index.js
@@ -11,10 +11,55 @@ app.use(express())
 
 
 
-app.get('/', (req, res) =>{
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gspcn8d.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+       
+        const productCollection = client.db('emaJohn').collection('products');
+
+
+        app.get('/products', async(req, res) =>{
+            const result = await productCollection.find().toArray();
+
+            res.send(result);
+        })
+
+        app.get('/totalProducts', async(req, res)=>{
+            const result = await productCollection.estimatedDocumentCount();
+            res.send({totalProducts: result});
+        })
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
+app.get('/', (req, res) => {
     res.send('Ema is shopping and wasting money')
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Ema is running on the port ${port}`);
 })
